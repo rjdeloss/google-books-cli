5dbf213a5b603c1ee9dddfed862503e0776dcb03
Rafiluis De Los Santos <rjdeloss@gmail.com>
2021-12-24 06:32:11 -0500

    added Chai dependency for smoother testing

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+
+describe('Google Books CLI Test Cases', () => {
+    describe('API Key', () => {
+        it('should be passed as a string into the http request', () => {
+            assert('key goes here', 'string')
+        })
+    })
+})
