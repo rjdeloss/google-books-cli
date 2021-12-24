8a73bb34d37cf341477e7c53a3531db06bad92c0
Rafiluis De Los Santos <rjdeloss@gmail.com>
2021-12-24 06:57:22 -0500

    create test cases to be tested

--- test/test.js
+++ test/test.js
@@ -1,9 +1,46 @@
 var assert = require('chai').assert;
 
 describe('Google Books CLI Test Cases', () => {
-    describe('API Key', () => {
-        it('should be passed as a string into the http request', () => {
-            assert('key goes here', 'string')
+    describe('Google Books API Key', () => {
+        it('should be string', () => {
+            // replace with api key
+            assert.typeOf('key goes here', 'string')
+        })
+        it('should be passed into the http request', () => {
+            // replace with api key
+            assert.typeOf('key goes here', 'string')
+        })
+    })
+    /** Tests for query */
+    describe('Search query for books', () => {
+        it('should take in a string as the parameter', () => {
+            assert.typeOf('query string goes here', 'string')
+        })
+        it('should return a list of 5 books matching the query', () => {
+            // replace with the response array length
+            assert.lengthOf('fives', 5)
+        })
+    })
+    /** Test for display of items on the list  */
+    describe('Result display format', () => {
+        it("should display the book's author", () => {
+            //replace with the object key for author
+            assert.equal('author', 'author')
+        })
+        it("should display the book's title", () => {
+            //replace with the object key for title
+            assert.equal('title', 'title')
+        })
+        it("should display the book's publishing company", () => {
+            //replace with the object key for publishing company
+            assert.equal('publishing company', 'publishing company')
+        })
+    })
+
+    describe('Selecting a book from the displayed items', () => {
+        it('should be able to select a book from the 5 displayed items', () => {
+            //replace with the code being tested
+            assert.equal(1,1)
         })
     })
 })
