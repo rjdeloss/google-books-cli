a1b756e8ccd5c30d2c9fdc7ef10068ef624834a6
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-01-12 14:26:36 -0500

    separate list and storage methods

--- utils/list.js
+++ utils/list.js
@@ -1,41 +1,4 @@
-import fs from 'fs';
-const storageFile = "storage.json"
-
-const createStorage = () => {
-    try {
-        if (!fs.existsSync(storageFile)) {
-            const storageObj = {
-                currentSearch: null,
-                list: []
-            }
-            
-            writeStorageData(storageObj)
-            console.log("storage created")
-        } 
-    } catch (err) {
-        console.log(err)
-    }
-}
-
-const readStorageData = () => {
-    const storage = fs.readFileSync(storageFile, "utf8")
-    const storageData = JSON.parse(storage)
-
-    return storageData
-}
-
-const writeStorageData = (data) => {
-    fs.writeFile(storageFile, JSON.stringify(data, null, 2), err => {
-        if (err) throw err;
-    })
-    console.log("data has been written")
-}
-
-const saveCurrentSearch = (data) => {
-    const currentData = readStorageData()
-    currentData.currentSearch = data;
-    writeStorageData(currentData)
-}
+import { readStorageData, writeStorageData } from "./storage.js";
 
 const saveToListByOrder = (order) => {
     if (Object.keys(order).length === 0) {
@@ -74,5 +37,4 @@ const renderList = () => {
         list.forEach(book => console.log(book))
 }
 
-
-export {createStorage, readStorageData, writeStorageData, saveCurrentSearch}
+export {saveToListByOrder, renderList}
