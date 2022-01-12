523ecef87ae41216d25f27e133ab0c1bf0a52d22
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-01-12 14:51:19 -0500

    add export storageFile

--- utils/storage.js
+++ utils/storage.js
@@ -37,42 +37,4 @@ const saveCurrentSearch = (data) => {
     writeStorageData(currentData)
 }
 
-const saveToListByOrder = (order) => {
-    if (Object.keys(order).length === 0) {
-        console.log("Oops... seems like you're missing options. use '-o <order>' or '-order <order>' ")
-        return;
-    }
-
-    const storageData = readStorageData()
-    const {currentSearch, list} = storageData
-    const { Order } = order
-
-    if (Order >= 0 && Order < 5) {
-        const bookFound = list.find(book => (book.id === currentSearch[Order].id))
-        if (list.length === 0) {
-            list.push(currentSearch[Order])
-            writeStorageData(storageData)
-            console.log("Book has been saved to your list")
-        } else if (bookFound && bookFound.id === currentSearch[Order].id) {
-            console.log("Book already exists in your list.")
-        } else {
-            list.push(currentSearch[Order])
-            writeStorageData(storageData)
-            console.log("Book has been saved to your list")
-        }
-    } else {
-        console.log("Something went wrong... please select an Order number between 0 and 4");
-    }
-}
-
-const renderList = () => {
-    const storageData = readStorageData()
-    const { list } = storageData
-    
-    list.length === 0 ? 
-        console.log("Oops... There are currently no saved books in your list.") : 
-        list.forEach(book => console.log(book))
-}
-
-
 export {createStorage, readStorageData, writeStorageData, saveCurrentSearch, storageFile}
